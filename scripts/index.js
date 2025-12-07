a6d68f3dc668bb26444db01655cc7fd81d9df392
Charles Marques <charlesrochamarques@gmail.com>
2025-12-07 14:13:34 -0300

    ADD like, removeButton and newCards

--- scripts/index.js
+++ scripts/index.js
@@ -34,10 +34,12 @@ console.log(containerElement);
 
 /*Criando Variavel para o Botão Editar Perfil*/
 const editButton = document.querySelector(".profile__edit-button");
-/*Criando Variavel para o Botão Fechar Editar Perfil*/
-const closeEditButton = document.querySelector(".popup__close");
+
 /*Variavel do Popup de Editar*/
 const editModal = document.querySelector("#edit-popup");
+
+/*Criando Variavel para o Botão Fechar Editar Perfil*/
+const closeEditButton = editModal.querySelector(".popup__close");
 
 /*Variaveis Titulo e Descrição Existentes*/
 const profileTitle = document.querySelector(".profile__title");
@@ -58,21 +60,38 @@ const cardTemplate = document
 /*Local onde os Cards Serão Criados*/
 const cardsContainer = document.querySelector(".cards__list");
 
+/*Variavel do New Popup*/
+const newModal = document.querySelector("#new-card-popup");
+
+/*Criando Variavel para o Botão NewCard*/
+const newCardButton = document.querySelector(".profile__add-button");
+
+/*Criando Variavel para o Botão Fechar NewCard*/
+const closeNewCardButton = newModal.querySelector(".popup__close");
+
+/*Variavel do Formulario*/
+const newCardForm = newModal.querySelector(".popup__form");
+
+/*Variaveis Name e Url dos newCards*/
+let cardNameInput = document.querySelector(".popup__input_type_card-name");
+let urlInput = document.querySelector(".popup__input_type_url");
+
 /* Funçao Abrir PopUp */
 
-function openModal(arg) {
+function openModal(arg, modal) {
   arg.addEventListener("click", function () {
-    editModal.classList.add("popup_is-opened");
+    modal.classList.add("popup_is-opened");
   });
 }
 
 /* Funçao Fechar PopUp */
 
-function closeModal(arg) {
+function closeModal(arg, modal) {
   arg.addEventListener("click", function () {
-    editModal.classList.remove("popup_is-opened");
+    modal.classList.remove("popup_is-opened");
   });
 }
+
 /* Funçao Para preencher os Campos nome e descrição */
 function fillProfileForm() {
   nameInput.value = profileTitle.textContent;
@@ -81,7 +100,7 @@ function fillProfileForm() {
 /* Funçao Para abrir o modo de edição e preencher os Campos nome e descrição */
 function handleOpenEditModal() {
   fillProfileForm();
-  openModal(editButton);
+  openModal(editButton, editModal);
 }
 /* Funçao Para Alterar os Campos nome e descrição e depois salvar*/
 
@@ -94,17 +113,28 @@ function handleProfileFormSubmit(evt) {
   editModal.classList.remove("popup_is-opened");
 }
 
-/*Abrindo o Popup de Editar*/
-
-handleOpenEditModal();
+/* Abrir popup de editar */
+editButton.addEventListener("click", handleOpenEditModal);
 
 /*Fechando o Popup de Editar*/
 
-closeModal(closeEditButton);
+closeModal(closeEditButton, editModal);
 
 /*Salvando as Informacoes*/
+openModal(newCardButton, newModal);
+closeModal(closeNewCardButton, newModal);
 
 editForm.addEventListener("submit", handleProfileFormSubmit);
+
+function handleLikeButton(evt) {
+  evt.target.classList.toggle("card__like-button_is-active");
+}
+function removeCard(evt) {
+  const cardElement = evt.target.closest(".card");
+  if (cardElement) {
+    cardElement.remove();
+  }
+}
 
 /*Criando Função para Gerar Cards*/
 function getCardElement(name, link) {
@@ -121,11 +151,19 @@ function getCardElement(name, link) {
   const nameElement = cardElement.querySelector(".card__title");
   nameElement.textContent = name;
 
-  const altElement = cardElement.querySelector(".card__image");
-  altElement.alt = name;
+  const imageElement = cardElement.querySelector(".card__image");
+  imageElement.alt = name;
 
-  const srcElement = cardElement.querySelector(".card__image");
-  srcElement.src = link;
+  imageElement.src = link;
+
+  /*1. Seleciona o botão de like dentro do card*/
+  const likeButton = cardElement.querySelector(".card__like-button");
+  /*2. ao clicar muda a classe do botao curtir*/
+  likeButton.addEventListener("click", handleLikeButton);
+
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  /*1. ao clicar remove o cartao da DOM*/
+  deleteButton.addEventListener("click", removeCard);
 
   return cardElement;
 }
@@ -138,3 +176,23 @@ function renderCard(name, link, container) {
 initialCards.forEach((item) => {
   renderCard(item.name, item.link, cardsContainer);
 });
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  const name = cardNameInput.value;
+  const url = urlInput.value;
+  console.log("test:" + name);
+  console.log("url:" + url);
+  console.log("cards:" + cardsContainer);
+  // Salva no array
+  initialCards.unshift({ name: name, link: url });
+
+  // Renderiza o card visivelmente
+  renderCard(name, url, cardsContainer);
+
+  newCardForm.reset();
+
+  newModal.classList.remove("popup_is-opened");
+}
+newCardForm.addEventListener("submit", handleCardFormSubmit);
